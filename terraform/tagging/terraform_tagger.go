43339272dc192ce21bfc895e9a6c176c0f6db86a
tronxd <tronxd@users.noreply.github.com>
2021-02-10 15:14:28 +0200

    TF tagger implementation (#10)
    * Initial commit
    
    * terraform tagger logic
    
    * UTs
    
    * remove comment
    
    * PR comments
    
    * re-arrange interface method order
    
    Co-authored-by: Nimrod Kor <nimrodkor@gmail.com>

--- terraform/tagging/terraform_tagger.go
+++ terraform/tagging/terraform_tagger.go
@@ -4,12 +4,20 @@ import (
 	"bridgecrewio/yor/common/git_service"
 	"bridgecrewio/yor/common/structure"
 	"bridgecrewio/yor/common/tagging"
+	"fmt"
 )
 
 type TerraformTagger struct {
 	tagging.Tagger
 }
 
-func (t *TerraformTagger) CreateTagsForBlock(block structure.IBlock, gitBlame *git_service.GitBlame) {
-	print(block, gitBlame)
+func (t *TerraformTagger) CreateTagsForBlock(block structure.IBlock, gitBlame *git_service.GitBlame) error {
+	for _, tag := range t.Tags {
+		err := tag.CalculateValue(gitBlame)
+		if err != nil {
+			return fmt.Errorf("failed to calculate tag value of tag %v, err: %s", tag, err)
+		}
+	}
+	block.AddNewTags(t.Tags)
+	return nil
 }
