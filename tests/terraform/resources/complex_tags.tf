430a4064c5aaccf0c68555eb8c11afe82f885e01
rotemavni <rotemavn@gmail.com>
2021-02-11 11:06:28 +0200

    .

--- tests/terraform/resources/complex_tags.tf
+++ tests/terraform/resources/complex_tags.tf
@@ -15,5 +15,17 @@ resource "aws_vpc" "vpc_tags_one_line" {
 }
 
 resource "aws_alb" "alb_with_merged_tags" {
-  tags = {}
+  tags = merge({"Name": "tag-for-s3", "Environment":"prod"},
+  {"yor_trace": "4329587194",
+    "git_org": "bana"})
+}
+
+resource "aws_instance" "many_instance_tags" {
+  ami = ""
+  instance_type = ""
+  tags =  merge({"Name": "tag-for-s3", "Environment":"prod"},
+  {"Owner": "bridgecrew"
+  },
+  {"yor_trace": "4329587194",
+    "git_org": "bana"})
 }
