8349f8756159e94073e126cab52a648a71a09213
Nimrod Kor <nimrodkor@gmail.com>
2021-02-14 13:24:58 +0200

    Refactor printing of new resources to add keys and values (#15)
    * Refactor printing of new resources to add keys and values
    
    * Rename methods to make things clearer
    
    * Extend tests on Report struct + add godocs on printing method
    
    * Refactor tests
    
    * Add cli output validation

--- tests/utils/utils.go
+++ tests/utils/utils.go
@@ -2,9 +2,14 @@ package utils
 
 import (
 	"bridgecrewio/yor/common/git_service"
+	"bytes"
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/stretchr/testify/assert"
+	"io"
+	"log"
+	"os"
+	"sync"
 	"testing"
 	"time"
 )
@@ -48,4 +53,34 @@ func ExtractDate(dateStr string) (time.Time, error) {
 	layout := "2006-01-02T15:04:05.000Z"
 	parsedDate, err := time.Parse(layout, dateStr)
 	return parsedDate, err
+}
+
+func CaptureOutput(f func()) string {
+	reader, writer, err := os.Pipe()
+	if err != nil {
+		panic(err)
+	}
+	stdout := os.Stdout
+	stderr := os.Stderr
+	defer func() {
+		os.Stdout = stdout
+		os.Stderr = stderr
+		log.SetOutput(os.Stderr)
+	}()
+	os.Stdout = writer
+	os.Stderr = writer
+	log.SetOutput(writer)
+	out := make(chan string)
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+	go func() {
+		var buf bytes.Buffer
+		wg.Done()
+		io.Copy(&buf, reader)
+		out <- buf.String()
+	}()
+	wg.Wait()
+	f()
+	writer.Close()
+	return <-out
 }
