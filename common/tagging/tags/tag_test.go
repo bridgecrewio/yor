4522b8575abf6ccdf43bb25ad885572cf665dbd9
Nimrod Kor <nimrodkor@gmail.com>
2021-02-14 10:36:02 +0200

    Basic logging service (#16)
    * Basic logging service
    
    * Code cleanup
    
    * Panic instead of Fatal to make testable

--- common/tagging/tags/tag_test.go
+++ common/tagging/tags/tag_test.go
@@ -1,8 +1,7 @@
-package tagging
+package tags
 
 import (
 	"bridgecrewio/yor/common/git_service"
-	"bridgecrewio/yor/common/tagging/tags"
 	"bridgecrewio/yor/tests/utils"
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -11,56 +10,56 @@ import (
 func TestTagCreation(t *testing.T) {
 	blame := utils.SetupBlame(t)
 	t.Run("BcTraceTagCreation", func(t *testing.T) {
-		tag := tags.YorTraceTag{}
+		tag := YorTraceTag{}
 		EvaluateTag(t, &tag, blame)
 		assert.Equal(t, "yor_trace", tag.Key)
 		assert.Equal(t, 36, len(tag.Value))
 	})
 
 	t.Run("GitOrgTagCreation", func(t *testing.T) {
-		tag := tags.GitOrgTag{}
+		tag := GitOrgTag{}
 		EvaluateTag(t, &tag, blame)
 		assert.Equal(t, "git_org", tag.Key)
 		assert.Equal(t, utils.Org, tag.Value)
 	})
 
 	t.Run("GitRepoTagCreation", func(t *testing.T) {
-		tag := tags.GitRepoTag{}
+		tag := GitRepoTag{}
 		EvaluateTag(t, &tag, blame)
 		assert.Equal(t, "git_repo", tag.Key)
 		assert.Equal(t, utils.Repository, tag.Value)
 	})
 
 	t.Run("GitFileTagCreation", func(t *testing.T) {
-		tag := tags.GitFileTag{}
+		tag := GitFileTag{}
 		EvaluateTag(t, &tag, blame)
 		assert.Equal(t, "git_file", tag.Key)
 		assert.Equal(t, utils.FilePath, tag.Value)
 	})
 
 	t.Run("GitCommitTagCreation", func(t *testing.T) {
-		tag := tags.GitCommitTag{}
+		tag := GitCommitTag{}
 		EvaluateTag(t, &tag, blame)
 		assert.Equal(t, "git_commit", tag.Key)
 		assert.Equal(t, utils.CommitHash1, tag.Value)
 	})
 
 	t.Run("GitLastModifiedAtCreation", func(t *testing.T) {
-		tag := tags.GitLastModifiedAtTag{}
+		tag := GitLastModifiedAtTag{}
 		EvaluateTag(t, &tag, blame)
 		assert.Equal(t, "git_last_modified_at", tag.Key)
 		assert.Equal(t, "2020-03-28 21:42:46 +0000 UTC", tag.Value)
 	})
 
 	t.Run("GitLastModifiedByCreation", func(t *testing.T) {
-		tag := tags.GitLastModifiedByTag{}
+		tag := GitLastModifiedByTag{}
 		EvaluateTag(t, &tag, blame)
 		assert.Equal(t, "git_last_modified_by", tag.Key)
 		assert.Equal(t, "schosterbarak@gmail.com", tag.Value)
 	})
 
 	t.Run("GitModifiersCreation", func(t *testing.T) {
-		tag := tags.GitModifiersTag{}
+		tag := GitModifiersTag{}
 		EvaluateTag(t, &tag, blame)
 		assert.Equal(t, "git_modifiers", tag.Key)
 		assert.Equal(t, "jonjozwiak/schosterbarak", tag.Value)
@@ -68,7 +67,7 @@ func TestTagCreation(t *testing.T) {
 
 }
 
-func EvaluateTag(t *testing.T, tag tags.ITag, blame git_service.GitBlame) {
+func EvaluateTag(t *testing.T, tag ITag, blame git_service.GitBlame) {
 	tag.Init()
 	err := tag.CalculateValue(&blame)
 	if err != nil {
