02ae73741fe0ba6c0ad1d609b51b024e802e745f
Nimrod Kor <nimrodkor@gmail.com>
2021-02-14 10:36:02 +0200

    Basic logging service (#16)
    * Basic logging service
    
    * Code cleanup
    
    * Panic instead of Fatal to make testable

---
+++ common/logging/logging_service_test.go
@@ -0,0 +1,39 @@
+package logger
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestLogger(t *testing.T) {
+	t.Run("Test logger setup", func(t *testing.T) {
+		assert.Equal(t, WARNING, Logger.logLevel)
+		Logger.Warning("Test warning")
+	})
+
+	t.Run("Test logger DEBUG logLevel setting", func(t *testing.T) {
+		Logger.SetLogLevel("DEBUG")
+		assert.Equal(t, DEBUG, Logger.logLevel)
+		Logger.Debug("Test debug")
+	})
+
+	t.Run("Test logger INFO logLevel setting", func(t *testing.T) {
+		Logger.SetLogLevel("INFO")
+		assert.Equal(t, INFO, Logger.logLevel)
+		Logger.Info("Test info")
+	})
+
+	t.Run("Test logger WARNING logLevel setting", func(t *testing.T) {
+		Logger.SetLogLevel("WARNING")
+		assert.Equal(t, WARNING, Logger.logLevel)
+		Logger.Warning("Test warning")
+	})
+
+	t.Run("Test logger ERROR logLevel setting", func(t *testing.T) {
+		Logger.SetLogLevel("ERROR")
+		assert.Equal(t, ERROR, Logger.logLevel)
+		assert.Panics(t, func() {
+			Logger.Error("Test error")
+		})
+	})
+}
