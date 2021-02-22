d8b0527bea0a27c85f9fa4532d80e944cd45c608
Nimrod Kor <nimrodkor@gmail.com>
2021-02-22 13:18:47 +0200

    Refactor tags' CalculateValue

--- common/tagging/tags/git_last_modified_at.go
+++ common/tagging/tags/git_last_modified_at.go
@@ -14,12 +14,11 @@ func (t *GitLastModifiedAtTag) Init() {
 	t.Key = "git_last_modified_at"
 }
 
-func (t *GitLastModifiedAtTag) CalculateValue(data interface{}) error {
+func (t *GitLastModifiedAtTag) CalculateValue(data interface{}) (ITag, error) {
 	gitBlame, ok := data.(*gitservice.GitBlame)
 	if !ok {
-		return fmt.Errorf("failed to convert data to *GitBlame, which is required to calculte tag value. Type of data: %s", reflect.TypeOf(data))
+		return nil, fmt.Errorf("failed to convert data to *GitBlame, which is required to calculte tag value. Type of data: %s", reflect.TypeOf(data))
 	}
 
-	t.Value = gitBlame.GetLatestCommit().Date.UTC().Format("2006-01-02 15:04:05")
-	return nil
+	return &Tag{Key: t.Key, Value: gitBlame.GetLatestCommit().Date.UTC().Format("2006-01-02 15:04:05")}, nil
 }
--- terraform/tagging/terraform_tagger.go
+++ terraform/tagging/terraform_tagger.go
@@ -17,12 +17,12 @@ type TerraformTagger struct {
 func (t *TerraformTagger) CreateTagsForBlock(block structure.IBlock, gitBlame *gitservice.GitBlame) {
 	var newTags []tags.ITag
 	for _, tag := range t.Tags {
-		err := tag.CalculateValue(gitBlame)
+		tag, err := tag.CalculateValue(gitBlame)
 		if err != nil {
 			logger.Warning(fmt.Sprintf("failed to calculate tag value of tag %v, err: %s", tag, err))
 			continue
 		}
-		newTags = append(newTags, &tags.Tag{Key: tag.GetKey(), Value: tag.GetValue()})
+		newTags = append(newTags, tag)
 	}
 	block.AddNewTags(newTags)
 }
