74402af87a5f0a1fd7dfe8c6be274faba451ca26
Nimrod Kor <nimrodkor@gmail.com>
2021-03-02 10:11:52 +0200

    Fix IT

--- tests/integration/tag_different_commits_test.go
+++ tests/integration/tag_different_commits_test.go
@@ -61,7 +61,7 @@ func TestMultipleCommits(t *testing.T) {
 
 		// run yor on resource 1
 		yorRunner := runner.Runner{}
-		err = yorRunner.Init(&common.Options{
+		err = yorRunner.Init(&common.TagOptions{
 			Directory: dir,
 			ExtraTags: "{}",
 		})
@@ -109,7 +109,7 @@ func TestMultipleCommits(t *testing.T) {
 
 		// run yor on both resources
 		yorRunner2 := runner.Runner{}
-		err = yorRunner2.Init(&common.Options{
+		err = yorRunner2.Init(&common.TagOptions{
 			Directory: dir,
 			ExtraTags: "{}",
 		})
