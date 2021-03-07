2b843908b1f268bf1079877becbcdd0698e8f1ee
nimrodkor <nimrodkor@gmail.com>
2021-03-07 14:35:24 +0200

    Add git service test for methods

--- src/common/gitservice/git_service_test.go
+++ src/common/gitservice/git_service_test.go
@@ -1,6 +1,7 @@
 package gitservice
 
 import (
+	"bridgecrewio/yor/src/common"
 	"bridgecrewio/yor/tests/utils"
 	"os"
 	"path/filepath"
@@ -41,5 +42,28 @@ func TestNewGitService(t *testing.T) {
 		gitService, err := NewGitService(terragoatPath)
 		assert.NotNil(t, err)
 		assert.Nil(t, gitService)
+	})
+
+	t.Run("Fail if gotten to root dir 2", func(t *testing.T) {
+		terragoatPath := utils.CloneRepo(utils.TerragoatURL)
+		defer os.RemoveAll(terragoatPath)
+
+		terragoatPath = filepath.Dir(filepath.Dir(terragoatPath))
+		gitService, err := NewGitService(terragoatPath)
+		assert.NotNil(t, err)
+		assert.Nil(t, gitService)
+	})
+
+	t.Run("Get blame for lines test", func(t *testing.T) {
+		terragoatPath := utils.CloneRepo(utils.TerragoatURL)
+		defer func() {
+			_ = os.RemoveAll(terragoatPath)
+		}()
+		gitService, _ := NewGitService(terragoatPath)
+
+		blame, _ := gitService.GetBlameForFileLines("terraform/aws/s3.tf", common.Lines{Start: 1, End: 13})
+		commit := blame.GetLatestCommit()
+		assert.Equal(t, 13, len(blame.BlamesByLine))
+		assert.Equal(t, "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0", commit.Hash.String())
 	})
 }
