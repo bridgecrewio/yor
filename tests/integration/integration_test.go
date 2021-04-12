055f52bc0a4fd907f58fa75de0086b92d0aaa382
nimrodkor <nimrodkor@gmail.com>
2021-04-12 22:37:15 +0300

    Fix & add IT

--- tests/integration/integration_test.go
+++ tests/integration/integration_test.go
@@ -183,7 +183,7 @@ func TestRunResults(t *testing.T) {
 }
 
 func TestTagUncommittedResults(t *testing.T) {
-	t.Run("Test terragoat tagging", func(t *testing.T) {
+	t.Run("Test tagging twice no result second time", func(t *testing.T) {
 		terragoatPath := utils.CloneRepo(utils.TerragoatURL)
 		outputPath := "./result_uncommitted.json"
 		defer func() {
@@ -195,6 +195,75 @@ func TestTagUncommittedResults(t *testing.T) {
 
 		// tag aws directory
 		tagDirectory(t, terragoatAWSDirectory)
+		// tag again, this time the files have uncommitted changes
+		tagDirectory(t, terragoatAWSDirectory)
+
+		terrraformParser := terraformStructure.TerrraformParser{}
+		terrraformParser.Init(terragoatAWSDirectory, nil)
+
+		dbAppFile := path.Join(terragoatAWSDirectory, "db-app.tf")
+		blocks, err := terrraformParser.ParseFile(dbAppFile)
+		failIfErr(t, err)
+		defaultInstanceBlock := blocks[0].(*terraformStructure.TerraformBlock)
+		if defaultInstanceBlock.GetResourceID() != "aws_db_instance.default" {
+			t.Errorf("invalid file structure, the resource id is %s", defaultInstanceBlock.GetResourceID())
+		}
+
+		rawTags := defaultInstanceBlock.HclSyntaxBlock.Body.Attributes["tags"]
+		rawTagsExpr := rawTags.Expr.(*hclsyntax.FunctionCallExpr)
+		assert.Equal(t, "merge", rawTagsExpr.Name)
+		mergeArgs := rawTagsExpr.Args
+		assert.Equal(t, 2, len(mergeArgs))
+		assert.Equal(t, 2, len(mergeArgs[0].(*hclsyntax.ObjectConsExpr).Items))
+		assert.Equal(t, 8, len(mergeArgs[1].(*hclsyntax.ObjectConsExpr).Items))
+
+		currentTags := defaultInstanceBlock.ExitingTags
+
+		expectedTagsValues := map[string]string{
+			"Name":                 "${local.resource_prefix.value}-rds",
+			"Environment":          "local.resource_prefix.value",
+			"git_last_modified_by": gitservice.GetGitUserEmail(),
+			"git_commit":           "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0",
+			"git_file":             strings.TrimPrefix(dbAppFile, terragoatPath+"/"),
+		}
+
+		for _, tag := range currentTags {
+			if expectedVal, ok := expectedTagsValues[tag.GetKey()]; ok {
+				assert.Equal(t, expectedVal, tag.GetValue(), fmt.Sprintf("Missmach in tag %s, expected %s, got %s", tag.GetKey(), expectedVal, tag.GetValue()))
+			}
+			if tag.GetKey() == "git_last_modified_at" {
+				timeTagValue, err := time.Parse("2006-01-02 15:04:05", tag.GetValue())
+				failIfErr(t, err)
+				diff := time.Now().UTC().Sub(timeTagValue)
+				assert.Greater(t, diff.Hours(), 100.)
+			}
+		}
+	})
+
+	t.Run("Test tagging after minor change", func(t *testing.T) {
+		terragoatPath := utils.CloneRepo(utils.TerragoatURL)
+		outputPath := "./result_uncommitted.json"
+		defer func() {
+			_ = os.RemoveAll(terragoatPath)
+			_ = os.RemoveAll(outputPath)
+		}()
+
+		terragoatAWSDirectory := path.Join(terragoatPath, "terraform/aws")
+
+		// tag aws directory
+		tagDirectory(t, terragoatAWSDirectory)
+
+		// Make minor change to file
+		input, _ := ioutil.ReadFile(path.Join(terragoatAWSDirectory, "db-app.tf"))
+		lines := strings.Split(string(input), "\n")
+		for i, line := range lines {
+			if line == "  instance_class          = \"db.t3.micro\"" {
+				lines[i] = "  instance_class          = \"db.t3.medium\""
+			}
+		}
+		output := strings.Join(lines, "\n")
+		_ = ioutil.WriteFile(path.Join(terragoatAWSDirectory, "db-app.tf"), []byte(output), 0644)
+
 		// tag again, this time the files have uncommitted changes
 		tagDirectory(t, terragoatAWSDirectory)
 
@@ -238,7 +307,6 @@ func TestTagUncommittedResults(t *testing.T) {
 				assert.True(t, diff < 2*time.Minute)
 			}
 		}
-
 	})
 }
 
