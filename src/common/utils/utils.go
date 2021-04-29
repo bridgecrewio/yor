1e6d7c5218c16971f0e52a5a5c11d7486092bb6b
nimrodkor <nimrodkor@gmail.com>
2021-04-29 16:47:09 +0300

    Remove cloudformation from dependency cycle in common

--- src/common/utils/utils.go
+++ src/common/utils/utils.go
@@ -3,16 +3,11 @@ package utils
 import (
 	"bridgecrewio/yor/src/common/logger"
 	"bridgecrewio/yor/src/common/structure"
-	"bridgecrewio/yor/src/common/types"
 	"bufio"
 	"fmt"
 	"os"
 	"reflect"
 )
-
-type YamlParser struct {
-	types.YamlParser
-}
 
 func InSlice(slice interface{}, elem interface{}) bool {
 	for _, e := range convertToInterfaceSlice(slice) {
@@ -38,10 +33,6 @@ func InSlice(slice interface{}, elem interface{}) bool {
 	return false
 }
 
-func (p *YamlParser) InSlice(slice interface{}, elem interface{}) bool {
-	return InSlice(slice, elem)
-}
-
 func getKind(val interface{}) reflect.Kind {
 	s := reflect.ValueOf(val)
 	return s.Kind()
@@ -60,26 +51,6 @@ func convertToInterfaceSlice(origin interface{}) []interface{} {
 	}
 
 	return ret
-}
-
-func (p *YamlParser) StructContainsProperty(s interface{}, property string) (bool, reflect.Value) {
-	var field reflect.Value
-	sValue := reflect.ValueOf(s)
-
-	// Check if the passed interface is a pointer
-	if sValue.Type().Kind() != reflect.Ptr {
-		// Create a new type of Iface's Type, so we have a pointer to work with
-		field = sValue.FieldByName(property)
-	} else {
-		// 'dereference' with Elem() and get the field by name
-		field = sValue.Elem().FieldByName(property)
-	}
-
-	if !field.IsValid() {
-		return false, field
-	}
-
-	return true, field
 }
 
 func GetFileScanner(filePath string, nonFoundLines *structure.Lines) (*bufio.Scanner, *structure.Lines) {
