d17d05a7bda111ad58006fc337e6cd44375c3281
Nimrod Kor <nimrodkor@gmail.com>
2021-05-04 11:03:09 +0300

    Checkout specific commit for test results to remain constant (#74)
    * Checkout specific commit for test results to remain constant
    
    * Fix integration test to checkout specific commit

--- tests/utils/utils.go
+++ tests/utils/utils.go
@@ -9,6 +9,7 @@ import (
 	"sync"
 
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 )
 
 const TerragoatURL = "https://github.com/bridgecrewio/terragoat.git"
@@ -43,16 +44,23 @@ func CaptureOutput(f func()) string {
 	return <-out
 }
 
-func CloneRepo(repoPath string) string {
+func CloneRepo(repoPath string, commitHash string) string {
 	dir, err := ioutil.TempDir("", "temp-repo")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// Clones the repository into the given dir, just as a normal git clone does
-	_, err = git.PlainClone(dir, false, &git.CloneOptions{
+	repo, err := git.PlainClone(dir, false, &git.CloneOptions{
 		URL: repoPath,
 	})
+
+	if commitHash != "" {
+		wt, _ := repo.Worktree()
+
+		commitRef := plumbing.NewHash(commitHash)
+		_ = wt.Checkout(&git.CheckoutOptions{Hash: commitRef})
+	}
 
 	if err != nil {
 		log.Fatal(err)
