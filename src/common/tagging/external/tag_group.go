54501cb38826c9df31dd3ed4e98a6e4f63011f4a
tronxd <tron47@gmail.com>
2021-05-31 00:45:55 +0300

    Full parsing

--- src/common/tagging/external/tag_group.go
+++ src/common/tagging/external/tag_group.go
@@ -1,7 +1,7 @@
 package external
 
 import (
-	"fmt"
+	"errors"
 	"io/ioutil"
 
 	"github.com/bridgecrewio/yor/src/common/logger"
@@ -15,12 +15,19 @@ type TagGroup struct {
 	tagging.TagGroup
 	configFilePath  string
 	config          map[interface{}]interface{}
-	tagGroupsByName map[string][]tags.ITag
+	tagGroupsByName map[string][]Tag
+}
+
+type Tag struct {
+	tags.ITag
+	defaultValue string
+	filters      map[interface{}]interface{}
+	matches      []interface{}
 }
 
 func (t *TagGroup) InitExternalTagGroups(configFilePath string) {
 	t.configFilePath = configFilePath
-	t.tagGroupsByName = make(map[string][]tags.ITag)
+	t.tagGroupsByName = make(map[string][]Tag)
 	t.InitExternalTagGroup()
 
 }
@@ -37,23 +44,24 @@ func (t *TagGroup) InitExternalTagGroup() {
 		logger.Error(err.Error())
 	}
 	t.config = configMap
-	externalTags := t.extractExternalTags()
-	t.SetTags(externalTags)
+	t.extractExternalTags()
+	//TODO
+	//t.SetTags(externalTags)
 }
 
-func (t *TagGroup) extractExternalTags() []tags.ITag {
-	externalGroupTags := make([]tags.ITag, 0)
+func (t *TagGroup) extractExternalTags() {
 	tagGroups := t.config["tag_group"]
 	switch tgs := tagGroups.(type) {
 	case map[interface{}]interface{}:
-		externalGroupTags = t.ExtractExternalGroupTags(tgs["tags"].([]interface{}))
+		groupName := tgs["name"].(string)
+		t.tagGroupsByName[groupName] = t.ExtractExternalGroupsTags(tgs["tags"].([]interface{}))
 	case []interface{}:
 		for _, tagGroup := range tgs {
-			fmt.Println(tagGroup)
-			//externalGroupTags = append(externalGroupTags, t.extractExternalTags(tg.([]interface{}))...)
+			tagGroupMap := tagGroup.(map[interface{}]interface{})
+			groupName := tagGroupMap["name"].(string)
+			t.tagGroupsByName[groupName] = t.ExtractExternalGroupsTags(tagGroupMap["tags"].([]interface{}))
 		}
 	}
-	return externalGroupTags
 }
 
 func (t *TagGroup) GetDefaultTags() []tags.ITag {
@@ -64,9 +72,35 @@ func (t *TagGroup) CreateTagsForBlock(block structure.IBlock) error {
 	return t.UpdateBlockTags(block, struct{}{})
 }
 
-func (t *TagGroup) ExtractExternalGroupTags(rawTags []interface{}) []tags.ITag {
-	var tagGroups tags.ITag
-	fmt.Println(tagGroups)
-	return nil
-	//TODO
+func (t *TagGroup) ExtractExternalGroupsTags(rawTags []interface{}) []Tag {
+	var groupTags []Tag
+	for _, rawTag := range rawTags {
+		rawTagMap := rawTag.(map[interface{}]interface{})
+		tagKey := rawTagMap["name"].(string)
+		groupFilters := rawTagMap["filters"].(map[interface{}]interface{})
+		tagValueObj := rawTagMap["value"]
+		computedTag, err := calculateExternalTag(tagValueObj.(map[interface{}]interface{}), tagKey, groupFilters)
+		if err != nil {
+			logger.Error(err.Error())
+		}
+		groupTags = append(groupTags, computedTag)
+	}
+	return groupTags
+}
+
+func calculateExternalTag(tagValueObj map[interface{}]interface{}, tagKey string, groupFilters map[interface{}]interface{}) (Tag, error) {
+	var calculatedTag = Tag{filters: groupFilters}
+	var okDefault, okMatches bool
+	var defaultValue, matches interface{}
+	if defaultValue, okDefault = tagValueObj["default"]; okDefault {
+		calculatedTag.defaultValue = defaultValue.(string)
+		calculatedTag.ITag = &tags.Tag{Key: tagKey, Value: defaultValue.(string)}
+	}
+	if matches, okMatches = tagValueObj["matches"]; okMatches {
+		calculatedTag.matches = matches.([]interface{})
+	}
+	if !okDefault && !okMatches {
+		return Tag{}, errors.New("please specify either a default tag value and/or a computed tag value")
+	}
+	return calculatedTag, nil
 }
