00193660c248483862c06e2ae96111adfcb683af
tronxd <tron47@gmail.com>
2021-06-01 10:58:29 +0300

    UT

---
+++ tests/terraform/resources/external_tagged/main.tf
@@ -0,0 +1,30 @@
+resource "aws_s3_bucket" "a" {
+  bucket = "my-tf-test-bucket"
+  acl = "private"
+
+  tags = {
+    Name = "My bucket"
+    Environment = "Production"
+  }
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-test-bucket"
+  acl = "private"
+
+  tags = {
+    Name = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+
+resource "aws_s3_bucket" "c" {
+  bucket = "my-tf-test-bucket"
+  acl = "private"
+}
+
+resource "aws_vpc" "d" {
+  cidr_block = ""
+}
+
