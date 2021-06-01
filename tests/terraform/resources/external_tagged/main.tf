396d6583f3fb98d28c9f730376e510d17b02c68b
tronxd <tron47@gmail.com>
2021-06-01 12:05:04 +0300

    tagged example

--- tests/terraform/resources/external_tagged/main.tf
+++ tests/terraform/resources/external_tagged/main.tf
@@ -7,16 +7,6 @@ resource "aws_s3_bucket" "a" {
   }
 }
 
-resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket"
-  acl = "private"
-
-  tags = {
-    Name = "My other bucket"
-  }
-}
-
-
 resource "aws_s3_bucket" "c" {
   bucket = "my-tf-test-bucket"
   acl = "private"
