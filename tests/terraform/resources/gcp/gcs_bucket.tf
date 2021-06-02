976931e5f22cae9773e245d25d6d7b9356ac9e97
nimrodkor <nimrodkor@gmail.com>
2021-06-02 10:53:27 +0300

    Fix resource

--- tests/terraform/resources/gcp/gcs_bucket.tf
+++ tests/terraform/resources/gcp/gcs_bucket.tf
@@ -1,18 +1,14 @@
-resource "google_storage_bucket" "static-site" {
-  name          = "image-store.com"
-  location      = "EU"
+resource "google_storage_bucket" "auto-expire" {
+  name          = "yor-test-gcs-bucket"
+  location      = "US"
   force_destroy = true
 
-  uniform_bucket_level_access = true
-
-  website {
-    main_page_suffix = "index.html"
-    not_found_page   = "404.html"
-  }
-  cors {
-    origin          = ["http://image-store.com"]
-    method          = ["GET", "HEAD", "PUT", "POST", "DELETE"]
-    response_header = ["*"]
-    max_age_seconds = 3600
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
   }
 }
