428632d00263bf80786e3dae98db5bfbd181b765
nimrodkor <nimrodkor@gmail.com>
2021-06-09 22:10:39 +0300

    Improve debug logging for parsing

--- src/common/parser.go
+++ src/common/parser.go
@@ -4,6 +4,7 @@ import "github.com/bridgecrewio/yor/src/common/structure"
 
 type IParser interface {
 	Init(rootDir string, args map[string]string)
+	Name() string
 	ParseFile(filePath string) ([]structure.IBlock, error)
 	WriteFile(readFilePath string, blocks []structure.IBlock, writeFilePath string) error
 	GetSkippedDirs() []string
