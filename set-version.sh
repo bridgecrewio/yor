1fc96ed16e91b2f33c1a2fd836b307d040aca64b
rotemavni <rotemavn@gmail.com>
2021-08-11 11:33:55 +0300

    add log

--- set-version.sh
+++ set-version.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "new tag: $GORELEASER_CURRENT_TAG"
+echo "Updating version file with new tag: $GORELEASER_CURRENT_TAG"
 echo "package common" > src/common/version.go
 echo "" >> src/common/version.go
 echo "const Version = \"$GORELEASER_CURRENT_TAG\"" >> src/common/version.go
