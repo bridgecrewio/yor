c6bc177040f873ae5619c10ebcfd66943abfc3f0
Rotem Avni <ravni@paloaltonetworks.com>
2022-11-22 17:13:23 +0200

    removed change to test file

--- tests/terraform/resources/tomap/tomap.tf
+++ tests/terraform/resources/tomap/tomap.tf
@@ -1,7 +1,5 @@
 resource "aws_security_group" "sample" {
   name = "${var.this}-sample"
 
-  tags = merge(var.tags, tomap({ "Name" = format("%s-sample", var.this) }), {
-    test_tag = "test_value"
-  })
+  tags = merge(var.tags, tomap({ "Name" = format("%s-sample", var.this) }))
 }
