02803cd60fbe84229ad138defe32aef6f87cdc9e
Rotem Avni <ravni@paloaltonetworks.com>
2023-05-16 11:14:29 +0300

    created the yor_name tag

--- src/common/tagging/code2cloud/tag_group.go
+++ src/common/tagging/code2cloud/tag_group.go
@@ -1,6 +1,8 @@
 package code2cloud
 
 import (
+	"fmt"
+	"github.com/bridgecrewio/yor/src/common/logger"
 	"github.com/bridgecrewio/yor/src/common/structure"
 	"github.com/bridgecrewio/yor/src/common/tagging"
 	"github.com/bridgecrewio/yor/src/common/tagging/tags"
@@ -16,15 +18,33 @@ func (t *TagGroup) InitTagGroup(_ string, skippedTags []string, explicitlySpecif
 	}
 	t.SkippedTags = skippedTags
 	t.SpecifiedTags = explicitlySpecifiedTags
-	t.SetTags([]tags.ITag{&YorTraceTag{}})
+	t.SetTags([]tags.ITag{&YorTraceTag{}, &YorNameTag{}})
 }
 
 func (t *TagGroup) GetDefaultTags() []tags.ITag {
 	return []tags.ITag{
 		&YorTraceTag{},
+		&YorNameTag{},
 	}
 }
 
 func (t *TagGroup) CreateTagsForBlock(block structure.IBlock) error {
-	return t.UpdateBlockTags(block, struct{}{})
+	return t.UpdateBlockTags(block)
+}
+
+func (t *TagGroup) UpdateBlockTags(block structure.IBlock) error {
+	var newTags []tags.ITag
+	var err error
+	var tagVal tags.ITag
+	for _, tag := range t.GetTags() {
+		tagVal, err = tag.CalculateValue(block)
+		if err != nil {
+			logger.Error(fmt.Sprintf("Failed to create %v tag for block %v", tag.GetKey(), block.GetResourceID()))
+		}
+		if tagVal != nil && tagVal.GetValue() != "" {
+			newTags = append(newTags, tagVal)
+		}
+	}
+	block.AddNewTags(newTags)
+	return err
 }
