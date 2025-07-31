bb659e1ae8e0657b0159b9218781bcfc8140ca7f
Daniel Exner <Daniel.Exner@jambit.com>
2025-07-31 11:19:25 +0200

    + add scaleway to providers block

--- src/terraform/structure/terraform_block.go
+++ src/terraform/structure/terraform_block.go
@@ -13,7 +13,7 @@ type TerraformBlock struct {
 	HclSyntaxBlock *hclsyntax.Block
 }
 
-var ProviderToTagAttribute = map[string]string{"aws": "tags", "azurerm": "tags", "google": "labels", "oci": "freeform_tags", "alicloud": "tags"}
+var ProviderToTagAttribute = map[string]string{"aws": "tags", "azurerm": "tags", "google": "labels", "oci": "freeform_tags", "alicloud": "tags", "scaleway": "tags"}
 
 const ResourceBlockType = "resource"
 const ModuleBlockType = "module"
